eb77ddeec99269f5a52912e370e58474c2176963
Seonjin Lee <sjbase@me.com>
2023-04-29 20:27:05 +0900

    init

---
+++ .config/nvim/lua/theme.lua
@@ -0,0 +1,7 @@
+-- local ok, _ = pcall(require, 'gruvbox')
+-- if not ok then return end
+
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_transparent_background = 1
+
+vim.cmd("colorscheme gruvbox-material")
