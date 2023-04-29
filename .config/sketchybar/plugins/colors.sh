a222dbaab6983bed150b24953bfed35bece883e4
Seonjin Lee <sjbase@me.com>
2023-04-29 21:50:21 +0900

    added yabai confs

---
+++ .config/sketchybar/plugins/colors.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export BAR=0x0032302f
+export BG=0xff282828
+export FG=0xffd4be98
+export FGALT=0x44d4be98
+# export FGALT=0x77ffffff
+export RED=0xffea6962
+export REDALT=0x44ea6962
+export GREEN=0xffa9b665
+export GREENALT=0x44a9b665
+export BLUE=0xff7daea3
+export BLUEALT=0x447daea3
+export YELLOW=0xffd8a657
+export YELLOWALT=0x44d8a657
