15742d9f6e33333405389aae59cc6ed96ca4f4a0
Seonjin Lee <sjbase@me.com>
2023-05-08 22:46:02 +0900

    feat: rofi

--- .config/sketchybar/plugins/input.sh
+++ .config/sketchybar/plugins/input.sh
@@ -3,8 +3,8 @@
 SOURCE=$(defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID)
 
 case ${SOURCE} in
-'com.apple.keylayout.ABC') LABEL='en' ;;
-'com.apple.keylayout.2SetHangul') LABEL='ko' ;;
+'com.apple.keylayout.ABC') LABEL='us' ;;
+'com.apple.keylayout.2SetHangul') LABEL='kr' ;;
 esac
 
 sketchybar --set $NAME label="$LABEL"
