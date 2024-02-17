f23db7ec8ac093bfb2dd466d1d10c0d0c3e4f535
Seonjin Lee <sjbase@me.com>
2024-02-17 11:28:01 -0500

    fix cmp tab

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,9 +10,6 @@ return {
       -- optional
       {
         'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
       },
       { 'williamboman/mason-lspconfig.nvim' },
 
@@ -91,7 +88,13 @@ return {
       }
 
       local cmp = require('cmp')
-      local cmp_action = require('lsp-zero').cmp_action()
+      local luasnip = require('luasnip')
+      local has_words_before = function()
+        if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+      end
+
       local cmp_mappings = lsp.defaults.cmp_mappings({
         ["<CR>"] = vim.NIL,
 
@@ -132,7 +135,7 @@ return {
         mapping = cmp_mappings,
         -- VS Code style (icon first)
         formatting = {
-          format = function(entry, vim_item)
+          format = function(_, vim_item)
             vim_item.kind = string.format("%s %s", get_symbol(vim_item.kind), vim_item.kind)
             vim_item.abbr = string.sub(vim_item.abbr, 1, 20)
             return vim_item
