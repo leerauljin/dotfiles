b350325f3a7c4a3e7f04b2babbb199942865bbfb
Seonjin Lee <sjbase@me.com>
2024-09-23 15:10:13 -0400

    updated Sep

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -15,8 +15,8 @@ map("n", "gw", "*N")
 map("x", "gw", "*N")
 
 -- dealing with word wrap
-map("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, silent = true, expr = true })
-map("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, silent = true, expr = true })
+-- map("n", "k", "v:count == 0 ? 'gk' : 'k'", { noremap = true, silent = true, expr = true })
+-- map("n", "j", "v:count == 0 ? 'gj' : 'j'", { noremap = true, silent = true, expr = true })
 
 
 -- Diagnsotic jump can use `<c-o>` to jump back
